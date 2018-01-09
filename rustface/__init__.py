330880cf7f1d311132e22e1fcfa58683624fc1fb
Karl Hobley <karl@kaed.uk>
2018-01-09 13:39:22 +0000

    Initial commit

---
+++ rustface/__init__.py
@@ -0,0 +1,5 @@
+from rustface._native import ffi, lib
+
+
+def test():
+        return lib.a_function_from_rust()
---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+def build_native(spec):
+    # build an example rust library
+    build = spec.add_external_build(
+        cmd=['cargo', 'build', '--release'],
+        path='./rust'
+    )
+
+    spec.add_cffi_module(
+        module_path='rustface._native',
+        dylib=lambda: build.find_dylib('rustface', in_path='target/release'),
+        header_filename=lambda: build.find_header('rustface.h', in_path='target')
+    )
+
+setup(
+    name='rustface',
+    version='0.0.1',
+    packages=['rustface'],
+    zip_safe=False,
+    platforms='any',
+    setup_requires=['milksnake'],
+    install_requires=['milksnake'],
+    milksnake_tasks=[
+        build_native
+    ]
+)
