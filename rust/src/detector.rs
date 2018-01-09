3103ff23dc1d11164df8be5d4eb40b1efa3459ea
Karl Hobley <karl@kaed.uk>
2018-01-09 15:24:49 +0000

    Implement detector functions

---
+++ rust/src/detector.rs
@@ -0,0 +1,56 @@
+extern crate rustface;
+
+use std::ffi::CStr;
+use std::os::raw::{c_char, c_void};
+use std::borrow::Borrow;
+use std::boxed::Box;
+use std::mem;
+
+use rustface::{Detector, FaceInfo, ImageData};
+
+// As the detector is a boxed trait, we need to wrap it with
+// another boxed object as boxed traits require two pointers
+pub struct DetectorWrapper {
+    detector: Box::<Detector>,
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn detector_create(model_filename: *const c_char) -> *mut DetectorWrapper {
+    let model_filename = CStr::from_ptr(model_filename);
+
+    let detector = rustface::create_detector(model_filename.to_string_lossy().borrow()).unwrap();
+    Box::<DetectorWrapper>::into_raw(Box::new(DetectorWrapper { detector }))
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn detector_set_min_face_size(detector: *mut DetectorWrapper, min_face_size: u32) {
+    let mut detector  = Box::<DetectorWrapper>::from_raw(detector);
+    detector.detector.set_min_face_size(min_face_size);
+    mem::forget(detector);
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn detector_set_score_thresh(detector: *mut DetectorWrapper, thresh: f64) {
+    let mut detector  = Box::<DetectorWrapper>::from_raw(detector);
+    detector.detector.set_score_thresh(thresh);
+    mem::forget(detector);
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn detector_set_pyramid_scale_factor(detector: *mut DetectorWrapper, scale_factor: f32) {
+    let mut detector  = Box::<DetectorWrapper>::from_raw(detector);
+    detector.detector.set_pyramid_scale_factor(scale_factor);
+    mem::forget(detector);
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn detector_set_slide_window_step(detector: *mut DetectorWrapper, step_x: u32, step_y: u32) {
+    let mut detector  = Box::<DetectorWrapper>::from_raw(detector);
+    detector.detector.set_slide_window_step(step_x, step_y);
+    mem::forget(detector);
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn detector_destroy(detector: *mut DetectorWrapper) {
+    Box::<DetectorWrapper>::from_raw(detector);
+}
--- rustface/__init__.py
+++ rustface/__init__.py
@@ -1,5 +1,31 @@
-from rustface._native import ffi, lib
+import os
+
+from . import _native
+
+
+class Detector:
+    def __init__(self):
+        self._ptr = _native.lib.detector_create(os.path.join(os.path.dirname(__file__), 'model/seeta_fd_frontal_v1.0.bin').encode('UTF-8'))
+
+    def set_min_face_size(self, face_size):
+        _native.lib.detector_set_min_face_size(self._ptr, face_size)
+
+    def set_score_thresh(self, thresh):
+        _native.lib.detector_set_score_thresh(self._ptr, thresh)
+
+    def set_pyramid_scale_factor(self, scale_factor):
+        _native.lib.detector_set_pyramid_scale_factor(self._ptr, scale_factor)
+
+    def set_slide_window_step(self, step_x, step_y):
+        _native.lib.detector_set_slide_window_step(self._ptr, step_x, step_y)
+
+    def __delete__(self):
+        _native.lib.detector_destroy(self._ptr)
 
 
 def test():
-        return lib.a_function_from_rust()
+    detector = Detector()
+    detector.set_min_face_size(20)
+    detector.set_score_thresh(2.0)
+    detector.set_pyramid_scale_factor(0.8)
+    detector.set_slide_window_step(4, 4)
